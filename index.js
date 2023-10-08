65430ee2e5cc7af698a4e1183317beac3675946e
Sandra Felicio <mjpfelicia@gmail.com>
2023-10-08 08:00:38 -0300

    Número quadrado, em matemática, é um inteiro que pode ser escrito como o quadrado de outro número inteiro. Ou ainda se a raiz quadrada de um número inteiro for outro inteiro, o primeiro é um número quadrado.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // Implementar comando de repetição (de 1 até 20)
 
 // Implementar os comandos de decisão para verificar números quadrados pares ou ímpares
+
+let pares = 0
+let impares = 0
+
+for (let number = 1; number <= 20; number = number + 1) {
+    const quadrado =  number**2
+    console.log(quadrado);
+
+    if (quadrado % 2 === 0) {
+        pares = pares + quadrado
+    } else {
+        impares = impares + quadrado
+    }    
+}
+
+console.log(`A soma de quadrados pares: `, pares)
+console.log(`A soma de quadrados ímpares: `, impares)
